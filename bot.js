9f17519b785daeec1e4d7656bd580c4f7d4eec46
notAmine1 <48600977+notAmine1@users.noreply.github.com>
2019-03-15 19:21:26 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`TEXT`,"http://twitch.tv/Dream")
+client.user.setGame(`MGP_Amine`,"http://twitch.tv/II_NotAmine_II")
 client.user.setStatus("dnd")
 });
 
